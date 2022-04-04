1cb0dec3be970b00ce254d236f24eccb07e60ebe
JoelCAI <joelargentina12345@gmail.com>
2022-04-04 01:11:12 -0300

    Agregar archivos de proyecto.

---
+++ LaCadena3/Sistema.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaCadena3
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            string cadena;
+ 
+            cadena = Validador.ValidarString("\n Ingrese una frase");
+
+            Validador.ToUpper(cadena);
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
